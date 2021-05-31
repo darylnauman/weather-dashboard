ea66e8b3e1abd05acc9f6691296e866cad711162
Daryl Nauman <darylnauman@gmail.com>
2021-05-31 09:08:40 -0400

    created separate functions to get locations and weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,41 @@ var searchBtn = $('#search-button');
 
 var currentCity;
 
-function getWeather () {
+// use Open Weather 'One Call API' to get weather based on city coordinates
+function getWeather(data) {
+
+    var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${APIkey}`
+    fetch(requestUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+
+            // current temperature
+             var currentCityTemp = data.current.temp;
+             console.log(`Temp: ${currentCityTemp}`);
+
+            // current wind speed
+            var currentCityWind = data.current.wind_speed;
+            console.log(`Wind: ${currentCityWind}`);
+
+            // current humidity
+            var currentCityHumidity = data.current.humidity;
+            console.log(`Humidity: ${currentCityHumidity}`);
+
+            // current UV index
+            var currentCityUV = data.current.uvi;
+            console.log(`UV: ${currentCityUV}`);
+
+            // current weather icon
+            var currentCityWeatherIcon = data.current.weather[0].icon;
+            console.log(`Icon: ${currentCityWeatherIcon}`);
+        })
+}
+
+// use Open Weather 'Current weather data (API)' to get city coordinates to then send to 'One Call API' to get weather
+function getCoordinates () {
     var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${APIkey}`;
     var storedCities = JSON.parse(localStorage.getItem("cities")) || [];
 
@@ -26,101 +60,22 @@ function getWeather () {
         localStorage.setItem("cities", JSON.stringify(storedCities));
         return cityInfo;
       })
-      .then(function(data) {
-        console.log(`in final then of first fetch, city: ${data.city}`);
-        console.log(`in final then of first fetch, lon: ${data.lon}`);
-        console.log(`in final then of first fetch, lat: ${data.lat}`);
-
-        var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${APIkey}`
-        fetch(requestUrl)
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(data) {
-                console.log("in second fetch");
-                console.log(data);
-
-                // current temperature
-                 var currentCityTemp = data.current.temp;
-                 console.log(`Temp: ${currentCityTemp}`);
-
-                // current wind speed
-                var currentCityWind = data.current.wind_speed;
-                console.log(`Wind: ${currentCityWind}`);
-
-                // current humidity
-                var currentCityHumidity = data.current.humidity;
-                console.log(`Humidity: ${currentCityHumidity}`);
-
-                // current UV index
-                var currentCityUV = data.current.uvi;
-                console.log(`UV: ${currentCityUV}`);
-    
-                // current weather icon
-                var currentCityWeatherIcon = data.current.weather[0].icon;
-                console.log(`Icon: ${currentCityWeatherIcon}`);
-            })
-    })
+      .then(function (data) {
+        getWeather(data);
+      })
 }
 
-// handle city name to search for weather data submission
+// handle city name to first search for city coordinates
 function handleCityFormSubmit (event) {
     event.preventDefault();
 
     currentCity = cityInputEl.val().trim();
     console.log(currentCity);
 
-    getWeather();
+    getCoordinates();
 }
 
 searchBtn.on("click", handleCityFormSubmit);
 
-//     fetch(requestUrl)
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (data) {
-//         console.log(data);
-        
-//         // current weather icon
-//         var currentCityWeatherIcon = data.weather[0].icon;
-//         console.log(`Icon: ${currentCityWeatherIcon}`);
-        
-//         // current temperature
-//         var currentCityTemp = data.main.temp;
-//         console.log(`Temp: ${currentCityTemp}`);
-
-//         // current wind speed
-//         var currentCityWind = data.wind.speed;
-//         console.log(`Wind: ${currentCityWind}`);
-
-//         // current humidity
-//         var currentCityHumidity = data.main.humidity;
-//         console.log(`Humidity: ${currentCityHumidity}`);
-
-//         //   var currentCityUV = ;
-//         //   console.log(`UV: ${currentCityUV}`);
-
-//         // city coordinates for UV
-//         currentCityLon = data.coord.lon;
-//         console.log(`Lon: ${currentCityLon}`);
-
-//         currentCityLat = data.coord.lat;
-//         console.log(`Lat: ${currentCityLat}`);
-
-
-        
-//       });
-
-//       console.log(`outside of fetch - Lon: ${currentCityLon}`);
-//     //   requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentCityLat}}&lon=${currentCityLon}&appid=${APIkey}`;
-
-// storedCities = JSON.parse(localStorage.getItem("cities"));
-    // var lon = storedCities[storedCities.length].lon;
-
-    // console.log(`last line of getCoordinates, updated storedCities - ${storedCities}`);
-    // console.log(`last line of getCoordinates, updated lon - ${lon}`);
-    // 
-
-    // https://openweathermap.org/api/one-call-api
-    // https://openweathermap.org/weather-conditions
+// https://openweathermap.org/api/one-call-api
+// https://openweathermap.org/weather-conditions
